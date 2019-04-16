7a9655c7513f8373d56dd4a06a24f77955b48099
Ubuntu <ubuntu@ip-172-26-2-185.ec2.internal>
2019-04-16 07:05:04 +0000

    Test pass: generator.go

--- generator.go
+++ generator.go
@@ -2,31 +2,59 @@ package main
 
 import (
 	"fmt"
-//	"math/rand"
+	"math/rand"
 )
 
+type Edge struct {
+	tail		int
+	weight		float64
+}
+
 type Graph struct {
-	AdjList		[][]int
+	AdjList		[][]Edge
 	Size		int
 	AvgENum		int
 	Density		float64
+	MaxWeight	float64
 	Connected	bool
 }
 
 func main() {
-	g := NewUndirectedGraph(10, 2, -1, true)
+	g := NewUndirectedGraph(10, 2, 0.6, 10, true)
 	fmt.Print(g)
 }
 
-func NewUndirectedGraph(size int, avgenum int, density float64, connected bool) *Graph {
-	g := Graph{make([][]int, size), size, avgenum, density, connected}
+func NewUndirectedGraph(size int, avgenum int, density float64, maxweight float64, connected bool) *Graph {
+	g := Graph{make([][]Edge, size), size, avgenum, density, maxweight, connected}
 	for i := range g.AdjList {
-		g.AdjList[i] = make([]int, 0)
+		g.AdjList[i] = make([]Edge, 0)
 	}
+	// total number of edges need to create
+	totalEdges := 0
+	if avgenum >= 0 {totalEdges = avgenum*size/2}
+	// if both avgenum and density is non-negative, ignore avgenum 
+	if density >= 0 {totalEdges = int(density * float64((size-1)*size/2))}
 	if connected {
 		ConnectGraph(&g)
+		totalEdges -= size
 	}
+	BuildGraph(&g, totalEdges)
 	return &g
+}
+
+func BuildGraph(g *Graph, e int) {
+	hashFactor := g.Size
+	exsit := make(map[int]bool, 2*e)
+	rand.Seed(1)
+	for len(exsit)<2*e {
+		head := rand.Intn(g.Size)
+		tail := rand.Intn(g.Size)
+		if _, ok := exsit[head * hashFactor + tail]; !ok && head!=tail {
+			exsit[head * hashFactor + tail] = true
+			exsit[tail * hashFactor + head] = true 
+			g.add(head, tail)
+		}
+	}
 }
 
 func ConnectGraph(g *Graph) {
@@ -47,6 +75,8 @@ func ConnectGraph(g *Graph) {
 }
 
 func (g *Graph) add(a int, b int) {
-	g.AdjList[a] = append(g.AdjList[a], b)
-	g.AdjList[b] = append(g.AdjList[b], a) 
+	//rand.Seed(1)
+	w := rand.Float64()*g.MaxWeight
+	g.AdjList[a] = append(g.AdjList[a], Edge{b, w})
+	g.AdjList[b] = append(g.AdjList[b], Edge{a, w}) 
 }
--- generator_test.go
+++ generator_test.go
@@ -10,38 +10,42 @@ func TestGenerator(t *testing.T) {
 		Size 		int
 		AvgENum 	int
 		Density		float64
+		MaxWeight	float64
 		Connected	bool
 	}{
-		{10, 2, -1.0, true},
-		{10, -1, 0.6, true},
-		{10, 2, -1.0, false},
-		{5000, 6, -1.0, true},
-		{5000, -1, 0.2, true},
+		{10, 2, -1.0, 10, true},
+		{10, -1, 0.6, 100, true},
+		{10, 2, -1.0, 1, false},
+		{5000, 6, -1.0, 25, true},
+		{5000, -1, 0.2, 64, true},
 	}
 	for _, tc := range testcases {
-		descr := fmt.Sprintf("Graph(size: %d, average edges: %d, density: %f, connected: %t)", tc.Size, tc.AvgENum, tc.Density, tc.Connected)
+		descr := fmt.Sprintf("Graph(size: %d, average edges: %d, density: %f, maxweight: %f, connected: %t)", tc.Size, tc.AvgENum, tc.Density, tc.MaxWeight, tc.Connected)
 		// get arributes
-		g := NewUndirectedGraph(tc.Size, tc.AvgENum, tc.Density, tc.Connected)
+		g := NewUndirectedGraph(tc.Size, tc.AvgENum, tc.Density, tc.MaxWeight, tc.Connected)
 		rSize := len(g.AdjList)
 		rTotalENum := 0
 		for _, vList := range g.AdjList {rTotalENum += len(vList)} 
 		rConnected := DFS(g)
-
+		// test size
 		if rSize != tc.Size {
 			t.Errorf("%q has %d vertices, expect %d", descr, rSize, tc.Size)
 		}
+		// test average number of edges 
 		if tc.AvgENum >= 0 {
 			rAvgENum := rTotalENum/rSize
 			if tc.AvgENum != rAvgENum {
 				t.Errorf("%q has %d edges per vertex, expect %d", descr, rAvgENum, tc.AvgENum)
 			}
 		}
+		// test density
 		if tc.Density >= 0 {
-			rDensity := float64(rTotalENum/(rSize*(rSize-1)))
+			rDensity := float64(rTotalENum)/float64(rSize*(rSize-1))
 			if rDensity != tc.Density {
 				t.Errorf("%q has %f edges, expect %f", descr, rDensity, tc.Density)
 			}
 		}
+		// test connected
 		if rConnected == false && tc.Connected == true {
 			t.Errorf("%q isn't connected, expect connected", descr)
 		}
@@ -50,6 +54,8 @@ func TestGenerator(t *testing.T) {
 
 func DFS(g *Graph) bool {
 	visited := make([]bool, len(g.AdjList))
+	// Traverse the graph, if the graph is connected, we should have seen all vertices 
+	// otherwise, it is un-connected
 	DFShelper(g.AdjList, 0, visited)
 	for _, seen := range visited {
 		if !seen {
@@ -59,11 +65,11 @@ func DFS(g *Graph) bool {
 	return true
 }
 
-func DFShelper(l [][]int, vertex int, visited []bool) {
+func DFShelper(l [][]Edge, vertex int, visited []bool) {
 	if visited[vertex] {return}
 	visited[vertex] = true
 	for _, next := range l[vertex] {
-		DFShelper(l, next, visited)
+		DFShelper(l, next.tail, visited)
 	}
 }
 
